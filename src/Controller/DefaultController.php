6f08957766713088770dc5c6b30dddc736a05558
Hlib Synkovskyi <gleb.sinkovskiy@gmail.com>
2019-07-01 21:07:20 +0300

    doctrine

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -12,13 +12,17 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="default")
      */
-    public function index() {
-        $message = 'I\'m index method!';
+    public function index(ProductRepository $productRepository)
+    {
+        $products = $productRepository->findby(
+            ['isTop' => true],
+            ['name' => 'ASC']
+        );
 
         return $this->render(
             'default/index.html.twig',
             [
-                'mess' => $message,
+                'products' => $products,
             ]
         );
     }
---
+++ src/Controller/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/product/{id}", name="product")
+     */
+    public function show(Product $product)
+    {
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+}
