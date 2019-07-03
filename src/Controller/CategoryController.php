6dd57b742329dfecc17bd0a16afae6cdbc65b1eb
Hlib Synkovskyi <gleb.sinkovskiy@gmail.com>
2019-07-03 19:16:49 +0300

    categories

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/category", name="category")
+     */
+    public function index(CategoryRepository $categoryRepository)
+    {
+        return $this->render('category/index.html.twig', [
+            'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/category/{id}", name="category_show")
+     */
+    public function show(Category $category)
+    {
+        return $this->render('category/show.html.twig', [
+            'category' => $category,
+        ]);
+    }
+
+}
